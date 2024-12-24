2c3b57b83bfc23c7dee39c81f57aa1472c67d447
Aditya Patil <160331022+adityapatil343@users.noreply.github.com>
2024-12-24 11:11:40 +0530

    Update keys_and_addresses.py

--- keys_and_addresses.py
+++ keys_and_addresses.py
@@ -1,13 +1,13 @@
 # keys_and_addresses.py
-# replace you key and address
- private_keys = [
- '138d72d23...'
- ]
+# replace you key and address 
+private_keys = [
+    'your_private_key_here'  # Example: '9da...365' 
+]
 
- my_addresses = [
-'0x0383AC...'
- ]
+my_addresses = [
+    'your_address_here'  # Example: '0x087....E85' 
+]
 
- labels = [
-'wallet1'
- ]
+labels = [
+    'wallet1' 
+]
